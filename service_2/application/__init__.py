f67dd2727d8b86543142120c51c0a6dda42ea835
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-03 20:41:49 +0000

    Configured service2

--- service_2/application/__init__.py
+++ service_2/application/__init__.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def service2():
-    cuisine = ["American", "British", "Caribbean", "Chinese", "French", "Greek", "Indian", "Italian", 
-    "Japanese", "Mediterranean", "Mexican", "Moroccan", "Spanish", "Thai", "Turkish", "Vietnamese"]
-    
+    western_cuisine = ["American", "British", "Caribbean", "French", "Greek", "Italian", "Mediterranean", "Mexican", "Moroccan", "Spanish"]
+    eastern_cuisine = ["Chinese", "Indian", "Japanese", "Thai", "Turkish", "Vietnamese", "Arabian", "Kurdish", "Lebanese", "Syrian"]
+    food = request.args.get("food")
+    print(food)
+    if food == "western":
+        cuisine = choice(western_cuisine)
+    else:
+        cuisine = choice(eastern_cuisine)
+    return cuisine
