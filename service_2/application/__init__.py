b2e005a276934011cd7628a0d758a54e189fed26
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-03 21:13:58 +0000

    Configured service_2

--- service_2/application/__init__.py
+++ service_2/application/__init__.py
@@ -4,7 +4,7 @@ from random import choice
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def service2():
+def home():
     western_cuisine = ["American", "British", "Caribbean", "French", "Greek", "Italian", "Mediterranean", "Mexican", "Moroccan", "Spanish"]
     eastern_cuisine = ["Chinese", "Indian", "Japanese", "Thai", "Turkish", "Vietnamese", "Arabian", "Kurdish", "Lebanese", "Syrian"]
     food = request.args.get("food")
