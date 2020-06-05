8a5df90116e9c68febf86a16bfd247438451d50e
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-05 14:42:12 +0000

    Update __init__.py

--- service_2/application/__init__.py
+++ service_2/application/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
-from random import choice
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+import random
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('SFIA2_DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    western_cuisine = ["American", "British", "Caribbean", "French", "Greek", "Italian", "Mediterranean", "Mexican", "Moroccan", "Spanish"]
-    eastern_cuisine = ["Chinese", "Indian", "Japanese", "Thai", "Turkish", "Vietnamese", "Arabian", "Kurdish", "Lebanese", "Syrian"]
-    food = request.args.get("food")
-    print(food)
-    if food == "western":
-        cuisine = choice(western_cuisine)
-    else:
-        cuisine = choice(eastern_cuisine)
-    return cuisine
+def home(id):
+    # food = request.args.get("food")
+    # print(food)
+    rand = random.randint(1, 10)
+    getCuisine = Western.query.filter_by(id=rand).first()
+    return getCuisine
+    
