25f554949b5d1152b91b48e7b60f83c951c804de
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-10 14:40:46 +0000

    Update create.py

--- service_2/create.py
+++ service_2/create.py
@@ -1,5 +1,5 @@
 from application import db
-from application.models import Western
+from application.models import Food
 
 db.drop_all()
 db.create_all()
--- service_3/create.py
+++ service_3/create.py
@@ -1,5 +1,5 @@
 from application import db
-from application.models import Soft
+from application.models import Drink
 
 db.drop_all()
 db.create_all()
