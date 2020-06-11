ddbbf1643615361641d9d1180748a05e2dda5115
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-11 18:50:47 +0000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,4 +2,9 @@
 . ~/.bashrc
 env SFIA2_DB_URI="${SFIA2_DB_URI}" 
 env SECRET_KEY="${SECRET_KEY}"
+docker-compose build
+docker push jortuk/service_1:latest
+docker push jortuk/service_2:latest
+docker push jortuk/service_3:latest
+docker push jortuk/service_4:latest
 docker stack deploy -c docker-compose.yaml sfia2
