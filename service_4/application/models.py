a1fd5406663e2795fd9cd50ed31e51cb7da6cfd2
Jortuk <Jordan.Taylor@academytrainee.com>
2020-06-14 18:34:40 +0000

    Update models.py

--- service_4/application/models.py
+++ service_4/application/models.py
@@ -1,7 +1,7 @@
 from application import db
 
 class Chow(db.Model):
-    __tablename__ = 'meals'
+    __tablename__ = 'chow'
     id = db.Column(db.Integer, primary_key=True)
     cuisine = db.Column(db.String(30), nullable=False)
     dessert = db.Column(db.String(30), nullable=False)
